e437e5e3652e12cb6f1fb52e4ea4abdbedfe1ad1
Manish chaudhary <manish.22b1531078@abes.ac.in>
2024-11-07 15:32:29 +0530

    modified

--- JS/login_script.js
+++ JS/login_script.js
@@ -1,40 +1,37 @@
 function validate(){
-    var user = document.querySelector('#user').value;
-    var pass = document.querySelector('#pass').value;
-
-    if(user != 'admin' || pass != 'admin'){
-        alert('login failed');
-    }else{
-        window.location.href = 'converter.html';
-    }
+    var user = document.querySelector("#user").value;
+    var pswd = document.querySelector("#pswd").value;
+    if(user != "admin" || pswd != "admin") alert("Login failed");
+    else window.location.href = "convertor.html";
 }
 
 let currency = {};
 
-fetch('https://api.exchange-api.com/v4/latest/USD')
+fetch('https://api.exchangerate-api.com/v4/latest/USD')
 .then(response => response.json())
-.then(data => {
+.then(data =>{
     currency = data.rates;
     const currencyKeys = Object.keys(currency);
-    currencyKeys.forEach((key) => {
-        const option = document.createElement('option');
+    currencyKeys.forEach((key) =>{
+        var option = document.createElement('option');
         option.value = key;
         option.innerText = key;
         document.getElementById('first').appendChild(option);
     });
-    currencyKeys.forEach((key) => {
-        const option = document.createElement('option');
+    currencyKeys.forEach((key) =>{
+        var option = document.createElement('option');
         option.value = key;
         option.innerText = key;
-        document.getElementById('first').appendChild(option);
+        document.getElementById('second').appendChild(option);
     });
-    const option = document.createdElement();
-})
+    const option = document.createElement()
+});
 
-const convert = () => {
-    const from = document.getElementById('first').value;
-    const to = document.getElementById('second').value;
+const convert = () =>{
+    const from = document.getElementById("first").value;
+    const to = document.getElementById("second").value;
     const amount = document.getElementById('amount').value;
     const result = (amount * currency[to]) / currency[from];
-    document.getElementById('other_currency').value = result;
+    document.getElementById("other_currency").value = result;
 }
+
